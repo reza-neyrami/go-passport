451e02ef19229ee0cf50be4aa9812a63fd5ac345
neyrami <neyrami.65@gmail.com>
2024-01-30 18:59:22 +0330

    service provider golaravel

--- PassportServiceProvider.go
+++ PassportServiceProvider.go
@@ -1,74 +1,92 @@
 package main
 
 import (
+
 	console "github.com/reza-neyrami/go-passport/src/Console"
-	"gorm.io/gorm"
+	guards "github.com/reza-neyrami/go-passport/src/Guards"
 )
-
-
 
 type PassportServiceProvider struct {
 }
 
-
 func (s *PassportServiceProvider) Boot(app *application.Application) {
-    // Register the routes
-    if config.Get("passport.registersRoutes") {
-        app.router.group(config.Get("passport.path", "oauth").As("passport"), func(r *router.Router) {
-            r.get("/authorize", controllers.AuthorizeController.Handle())
-            r.post("/authorize", controllers.AuthorizeController.Handle())
-            r.get("/token", controllers.AccessTokenController.Handle())
-            r.post("/token", controllers.AccessTokenController.Handle())
-            r.get("/userinfo", controllers.UserInfoController.Handle())
-        })
-    }
+	// Register the routes
+	if config.Get("passport.registersRoutes") {
+		app.router.group(config.Get("passport.path", "oauth").As("passport"), func(r *router.Router) {
+			r.get("/authorize", controllers.AuthorizeController.Handle())
+			r.post("/authorize", controllers.AuthorizeController.Handle())
+			r.get("/token", controllers.AccessTokenController.Handle())
+			r.post("/token", controllers.AccessTokenController.Handle())
+			r.get("/userinfo", controllers.UserInfoController.Handle())
+		})
+	}
 
-    // Register the resources
-    app.views.AddPath("passport", config.Get("passport.resourcesPath", "resources/views/passport"))
+	// Register the resources
+	app.views.AddPath("passport", config.Get("passport.resourcesPath", "resources/views/passport"))
 
-    // Register the migration files
-    if app.runningInConsole() && config.Get("passport.runsMigrations") && !config.Get("passport.clientUuids") {
-        app.migrations.AddPath(config.Get("passport.migrationsPath", "database/migrations"))
-    }
+	// Register the migration files
+	if app.runningInConsole() && config.Get("passport.runsMigrations") && !config.Get("passport.clientUuids") {
+		app.migrations.AddPath(config.Get("passport.migrationsPath", "database/migrations"))
+	}
 
-    // Register the publishable resources
-    if app.runningInConsole() {
-        app.publish(config.Get("passport.migrationsPath", "database/migrations"), "passport-migrations")
-        app.publish(config.Get("passport.configPath", "config/passport.php"), "passport-config")
-        app.publish(config.Get("passport.resourcesPath", "resources/views/passport"), "passport-views")
-    }
+	// Register the publishable resources
+	if app.runningInConsole() {
+		app.publish(config.Get("passport.migrationsPath", "database/migrations"), "passport-migrations")
+		app.publish(config.Get("passport.configPath", "config/passport.php"), "passport-config")
+		app.publish(config.Get("passport.resourcesPath", "resources/views/passport"), "passport-views")
+	}
 
-    // Register the commands
-    app.commands.Add(
-        console.InstallCommand,
-        console.ClientCommand,
-        console.HashCommand,
-        console.KeysCommand,
-        console.Purge(),
-    )
+	// Register the commands
+	app.commands.Add(
+		console.InstallCommand,
+		console.ClientCommand,
+		console.HashCommand,
+		console.KeysCommand,
+		console.PurgeCommand,
+	)
 
-    // Register the guard
-    app.auth.Extend("passport", func(auth *auth.Guard) *guards.TokenGuard {
-        return guards.NewTokenGuard(
-            auth,
-            models.NewClientRepository(),
-            models.NewAccessTokenRepository(),
-            models.NewRefreshTokenRepository(),
-            models.NewScopeRepository(),
-            jwt.NewParser(jwt.NewJoseEncoder()),
-            app.make("encrypter"),
-            app.make("request"),
-        )
-    })
+	// Register the guard
+	app.auth.Extend("passport", func(auth *auth.Guard) *guards.TokenGuard {
+		return guards.NewTokenGuard(
+			auth,
+			models.NewClientRepository(),
+			models.NewAccessTokenRepository(),
+			models.NewRefreshTokenRepository(),
+			models.NewScopeRepository(),
+			jwt.Parser{}, // Added import and initialized jwt.Parser
+			app.make("encrypter"),
+			app.make("request"),
+		)
+	})
 
-    // Delete the cookie on logout
-    if config.Get("passport.deleteCookieOnLogout") {
-        app.Events.Listen(events.Logout, func(event *events.LogoutEvent) {
-            if event.User != nil {
-                if cookie := event.User.GetToken(); cookie != nil {
-                    cookie.Delete()
-                }
-            }
-        })
-    }
+	// Delete the cookie on logout
+	if config.Get("passport.deleteCookieOnLogout") {
+		app.Events.Listen(events.Logout, func(event *events.LogoutEvent) {
+			if event.User != nil {
+				if cookie := event.User.GetToken(); cookie != nil {
+					cookie.Delete()
+				}
+			}
+		})
+	}
+}
+
+func (s *PassportServiceProvider) RegisterRoutes() {
+	if Passport.RegistersRoutes {
+		router := app.NewRouter()
+		router.Group("passport", func(r *router.Router) {
+			r.PathPrefix(config.Get("passport.path", "oauth")).Namespace("Laravel/Passport/Http/Controllers").LoadRoutesFrom(currentDirectoryPath() + "/../routes/web.go")
+		})
+		app.Router.Merge(router)
+	}
+}
+
+func (s *PassportServiceProvider) RegisterResources() {
+	app.Views.AddPath("passport", config.Get("passport.resourcesPath", "resources/views/passport"))
+}
+
+func (s *PassportServiceProvider) RegisterMigrations() {
+	if app.IsRunningInConsole() && Passport.RunsMigrations && !config.GetBool("passport.clientUuids") {
+		app.Migrations.AddPath(config.Get("passport.migrationsPath", "database/migrations"))
+	}
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,80 @@ module github.com/reza-neyrami/go-passport
 go 1.21.6
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/bytedance/sonic v1.10.2 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/containerd/continuity v0.3.0 // indirect
+	github.com/docker/cli v20.10.22+incompatible // indirect
+	github.com/docker/docker v20.10.24+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/gookit/filter v1.2.0 // indirect
+	github.com/gookit/goutil v0.6.12 // indirect
+	github.com/gookit/validate v1.5.1 // indirect
+	github.com/goravel/framework v1.13.1 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/opencontainers/runc v1.1.5 // indirect
+	github.com/ory/dockertest/v3 v3.10.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	github.com/savioxavier/termlink v1.3.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/unrolled/secure v1.13.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.9.1 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
+	google.golang.org/grpc v1.58.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/google/uuid v1.6.0
+	github.com/goravel/gin v1.1.6
 	github.com/jmoiron/sqlx v1.3.5
 	golang.org/x/crypto v0.18.0
 	gorm.io/gorm v1.25.6
